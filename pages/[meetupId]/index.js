cc65711cf57da624578ea389c8aa62b81ef2c5d4
huseyin.turkmenoglu <turkmenogluhuseyin@gmail.com>
2022-07-08 22:32:34 +0300

    Fixed info

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,11 +9,3 @@ const MeetupDetails = () => {
 }
 
 export default MeetupDetails
-
-{/* <img
-        src="https://images.unsplash.com/photo-1656711168736-4308ff309da8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&q=60"
-        alt="Test MEetup"
-      />
-      <h1>A Test Meetup</h1>
-      <address>Some Street 5, Some City, Some Country</address>
-      <p>The meetup description</p> */}
